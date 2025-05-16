d5267996efa77d607f52b0c03e28100f6530c3ca
Rohith2106 <109177721+Rohith2106@users.noreply.github.com>
2025-05-16 12:55:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '1234',
+  password: process.env.DB_PASSWORD || 'your sql pass',
   database: process.env.DB_NAME || 'todo_db',
   waitForConnections: true,
   connectionLimit: 10,
